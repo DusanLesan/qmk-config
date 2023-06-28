3262dd22ff974c4ceedf3da2c863747c7d585c95
Dusan <oalfodr@gmail.com>
2023-06-28 02:47:43 +0200

    sym td

--- keyboards/splitkb/kyria/keymaps/default/keymap.c
+++ keyboards/splitkb/kyria/keymaps/default/keymap.c
@@ -35,12 +35,17 @@ enum {
 };
 
 enum {
-  QUOT_LAYR = 0
+  NAV_SPC = 0,
+  SYM_ENT
 };
 
 int cur_dance (tap_dance_state_t *state);
-void ql_finished (tap_dance_state_t *state, void *user_data);
-void ql_reset (tap_dance_state_t *state, void *user_data);
+void td_finished (tap_dance_state_t *state, void *user_data, uint16_t layer, uint16_t tap_key);
+void td_reset (tap_dance_state_t *state, void *user_data, uint16_t layer);
+void nav_td_finished(tap_dance_state_t *state, void *user_data);
+void nav_td_reset(tap_dance_state_t *state, void *user_data);
+void sym_td_finished(tap_dance_state_t *state, void *user_data);
+void sym_td_reset(tap_dance_state_t *state, void *user_data);
 
 #define SYM      MO(_SYM)
 #define NAV      MO(_NAV)
@@ -49,36 +54,31 @@ void ql_reset (tap_dance_state_t *state, void *user_data);
 
 #define CTL_ESC  MT(MOD_LCTL, KC_ESC)
 #define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
-#define CTL_MINS MT(MOD_RCTL, KC_MINUS)
-#define ALT_ENT  MT(MOD_LALT, KC_ENT)
+#define SFT_ENT  MT(MOD_RSFT, KC_ENT)
 #define CTL_SPC  MT(MOD_LCTL, KC_SPC)
 #define SFT_SPC  MT(MOD_RSFT, KC_SPC)
-
-#define NAV_SPC  LT(NAV, KC_SPC)
-#define SYM_SPC  LT(SYM, KC_SPC)
-
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * Base Layer: QWERTY
  *
- * ,--------------------------------------------.                                 ,-------------------------------------------.
- * |  Tab   |   Q  |   W  |   E  |   R  |   T   |                                 |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
- * |--------+------+------+------+------+-------|                                 |------+------+------+------+------+--------|
- * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G   |                                 |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
- * |--------+------+------+------+------+-------+---------------.  ,--------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B   | [ {  | CapsLk |  |F-keys |  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
- * `----------------------+------+------+-------+------+--------|  |-------+------+------+------+------+----------------------'
- *                        | LGUI | LGUI | LAlt/ | Ctrl/ | Nav/  |  | Sym/  | Space| AltGr| RGUI | Menu |
- *                        |      |      | Enter | Space | Space |  | Space |      |      |      |      |
- *                        `-------------------------------------'  `-----------------------------------'
+ * ,-------------------------------------------.                              ,-------------------------------------------.
+ * |  Tab   |   Q  |   W  |  E   |  R   |  T   |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
+ * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
+ * |Ctrl/Esc|   A  |   S  |  D   |  F   |  G   |                              |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "|
+ * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |  C   |  V   |  B   | [ {  | Caps |  |F-keys|  ] } |   N  |   M  | ,  < | . >  | /  ? | RShift |
+ * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
+ *                        | Ctl  | GUI  | Alt  | Ctl/ | Nav/ |  | Sym/ | Sft/ | Alt  | RGUI | Menu |
+ *                        |      |      |      | Spc  | Spc  |  | Ent  | Ent  |      |      |      |
+ *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB , KC_Q, KC_W, KC_E   , KC_R   , KC_T   ,                                               KC_Y   , KC_U   ,  KC_I  ,   KC_O, KC_P   , KC_BSPC ,
-     CTL_ESC, KC_A, KC_S, KC_D   , KC_F   , KC_G   ,                                               KC_H   , KC_J   ,  KC_K  ,   KC_L, KC_SCLN, CTL_QUOT,
-     KC_LSFT, KC_Z, KC_X, KC_C   , KC_V   , KC_B   , KC_LBRC, KC_CAPS,             FKEYS, KC_RBRC, KC_N   , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT ,
-                          KC_LGUI, KC_LGUI, ALT_ENT, CTL_SPC, TD(QUOT_LAYR),     SYM_SPC, SFT_SPC, KC_RALT, KC_RGUI, KC_APP
+     KC_TAB , KC_Q, KC_W, KC_E   , KC_R   , KC_T   ,                                              KC_Y   , KC_U   , KC_I   , KC_O  , KC_P   , KC_BSPC ,
+     CTL_ESC, KC_A, KC_S, KC_D   , KC_F   , KC_G   ,                                              KC_H   , KC_J   , KC_K   , KC_L  , KC_SCLN, CTL_QUOT,
+     KC_LSFT, KC_Z, KC_X, KC_C   , KC_V   , KC_B   , KC_LBRC, KC_CAPS    ,        FKEYS, KC_RBRC, KC_N   , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT ,
+                          KC_LCTL, KC_LGUI, KC_LALT, CTL_SPC, TD(NAV_SPC),  TD(SYM_ENT), SFT_ENT, KC_RALT, KC_RGUI, KC_APP
     ),
 
 /*
@@ -160,45 +160,62 @@ int cur_dance (tap_dance_state_t *state) {
 }
 
 //Initialize tap structure associated with example tap dance key
-static tap ql_tap_state = {
+static tap td_tap_state = {
   .is_press_action = true,
   .state = 0
 };
 
 //Functions that control what our tap dance key does
-void ql_finished (tap_dance_state_t *state, void *user_data) {
-  ql_tap_state.state = cur_dance(state);
-  switch (ql_tap_state.state) {
+void td_finished(tap_dance_state_t *state, void *user_data, uint16_t layer, uint16_t tap_key) {
+  td_tap_state.state = cur_dance(state);
+  switch (td_tap_state.state) {
     case SINGLE_TAP:
-      tap_code(KC_SPC);
+      tap_code(tap_key);
       break;
     case SINGLE_HOLD:
-      layer_on(_NAV);
+      layer_on(layer);
       break;
     case DOUBLE_TAP:
       //check to see if the layer is already set
-      if (layer_state_is(_NAV)) {
+      if (layer_state_is(layer)) {
         //if already set, then switch it off
-        layer_off(_NAV);
-      } else { 
+        layer_off(layer);
+      } else {
         //if not already set, then switch the layer on
-        layer_on(_NAV);
+        layer_on(layer);
       }
       break;
   }
 }
 
-void ql_reset (tap_dance_state_t *state, void *user_data) {
+void td_reset(tap_dance_state_t *state, void *user_data, uint16_t layer) {
   //if the key was held down and now is released then switch off the layer
-  if (ql_tap_state.state==SINGLE_HOLD) {
-    layer_off(_NAV);
+  if (td_tap_state.state==SINGLE_HOLD) {
+    layer_off(layer);
   }
-  ql_tap_state.state = 0;
+  td_tap_state.state = 0;
 }
 
-//Associate our tap dance key with its functionality
+void nav_td_finished(tap_dance_state_t *state, void *user_data) {
+  td_finished(state, user_data, _NAV, KC_SPC);
+}
+
+void nav_td_reset(tap_dance_state_t *state, void *user_data) {
+  td_reset(state, user_data, _NAV);
+}
+
+void sym_td_finished(tap_dance_state_t *state, void *user_data) {
+  td_finished(state, user_data, _SYM, KC_ENT);
+}
+
+void sym_td_reset(tap_dance_state_t *state, void *user_data) {
+  td_reset(state, user_data, _SYM);
+}
+
+//Associate tap dance key with its functionality
 tap_dance_action_t tap_dance_actions[] = {
-  [QUOT_LAYR] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, ql_finished, ql_reset)
+  [NAV_SPC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, nav_td_finished, nav_td_reset),
+  [SYM_ENT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, sym_td_finished, sym_td_reset)
 };
 
 void keyboard_post_init_user(void) {
